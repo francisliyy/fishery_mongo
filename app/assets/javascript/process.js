f831bb17c0f22bb12118c43c40fa73012e123c2b
yingxin li <yli120@ace.cs.fiu.edu>
2018-02-23 15:53:21 -0500

    add process page

---
+++ app/assets/javascript/process.js
@@ -0,0 +1,132 @@
+function onNext(parent, panel) {
+  hash = "#" + panel.id;
+  $(".acc-wizard-sidebar",$(parent))
+      .children("li")
+      .children("a[href='" + hash + "']")
+      .parent("li")
+      .removeClass("acc-wizard-todo")
+      .addClass("acc-wizard-completed");
+}
+
+$(function() {
+
+	$(".acc-wizard").accwizard({onNext:onNext});
+
+    $('#projectionDate').datetimepicker({
+    	format:'MMMM Do YYYY',
+    }).on('dp.change', function(e) {
+    	
+    });
+
+    $('#fishingStartDate').datetimepicker({
+    	format:'MMMM Do YYYY',
+    });
+
+    $('#fishingEndDate').datetimepicker({
+    	format:'MMMM Do YYYY',
+    });
+
+	$("#mask").addClass('lmask');
+    $.ajax({
+    	url: $SCRIPT_ROOT+'/processview/getTableData',
+    	type: 'get',
+    	dataType: 'JSON',
+    	data: {param1: 'value1'},
+    })
+    .done(function(result) {
+    	$("#table-ibParam").bootstrapTable({
+	    	//url: $SCRIPT_ROOT+'/processview/getTableData/',         //请求后台的URL（*）
+	    	//dataType:'json',
+	    	data:result,
+	        method: 'get',                      //请求方式（*）
+	        toolbar: '#toolbar',                //工具按钮用哪个容器
+	        striped: true,                      //是否显示行间隔色
+	        cache: false,                       //是否使用缓存，默认为true，所以一般情况下需要设置一下这个属性（*）
+	        pagination: false,                   //是否显示分页（*）
+	        sortable: false,                     //是否启用排序
+	        sortOrder: "asc",                   //排序方式
+	        //queryParams: ibParamTable.queryParams,//传递参数（*）
+	        sidePagination: "server",           //分页方式：client客户端分页，server服务端分页（*）
+	        pageNumber:1,                       //初始化加载第一页，默认第一页
+	        pageSize: 10,                       //每页的记录行数（*）
+	        pageList: [10, 25, 50, 100],        //可供选择的每页的行数（*）
+	        search: false,                       //是否显示表格搜索，此搜索是客户端搜索，不会进服务端，所以，个人感觉意义不大
+	        strictSearch: true,
+	        showColumns: false,                  //是否显示所有的列
+	        showRefresh: false,                  //是否显示刷新按钮
+	        minimumCountColumns: 2,             //最少允许的列数
+	        clickToSelect: true,                //是否启用点击选中行
+	        height: 500,                        //行高，如果没有设置height属性，表格自动根据记录条数觉得表格高度
+	        uniqueId: "ID",                     //每一行的唯一标识，一般为主键列
+	        showToggle:false,                    //是否显示详细视图和列表视图的切换按钮
+	        cardView: false,                    //是否显示详细视图
+	        detailView: false,                   //是否显示父子表
+	    	columns:[
+	    		[
+	    			{
+	    				title:"East Year",
+	    				field:"E_Year",
+	    				editable:false,
+	    			},
+	    			{
+	    				title:"East Observed",
+	    				field:"E_Observed",
+	    				editable: {
+		                    type: 'text',
+		                    title: 'East Observed',
+		                    validate: function (v) {
+		                        if (isNaN(v)) return 'East Observed must be number';
+		                        var stockmean = parseFloat(v);
+		                        if (stockmean <= 0) return 'East Observed must larger than 0';
+			                }
+	                    }
+	    			},
+	    			{
+	    				title:"West Year",
+	    				field:"W_Year",
+	    				editable:false,
+	    			},
+	    			{
+	    				title:"West Observed",
+	    				field:"W_Observed",
+	    				editable:false,
+	    			},
+	    		],
+	    	],
+	    	onEditableSave: function (field, row, oldValue, $el) {
+	    		console.log(row);
+	    		$.ajax({
+                    type: "post",
+                    url: $SCRIPT_ROOT+"/processview/editTableData",
+                    data: row,
+                    dataType: 'json',
+                }).done(function(result) {
+                	console.log(result);
+                	if(result.status=='1'){
+                		alert('submit success');
+                	}
+                });
+	    	},
+	    });
+    })
+    .fail(function() {
+    	console.log("error");
+    })
+    .always(function() {
+    	console.log("complete");
+    	$("#mask").removeClass('lmask');
+    });
+
+    /*let ibParamTable = new Object();
+    //得到查询的参数
+    ibParamTable.queryParams = function (params) {
+        var temp = {   //这里的键的名字和控制器的变量名必须一直，这边改动，控制器也需要改成一样的
+            limit: params.limit,   //页面大小
+            offset: params.offset,  //页码
+            departmentname: $("#txt_search_departmentname").val(),
+            statu: $("#txt_search_statu").val()
+        };
+        //return temp;
+    };*/
+
+});
--- app/views.py
+++ app/views.py
@@ -1,16 +1,57 @@
-from flask import render_template
+from flask import render_template,jsonify,Response,request
 from flask.ext.appbuilder import ModelView
 from flask.ext.appbuilder.models.mongoengine.interface import MongoEngineInterface
 from app import appbuilder
+from flask_appbuilder import BaseView, expose, has_access
+from app.rutils import *
+import pandas as pd
+import numpy as np
+import json
 
 """
     Define you Views here
 """
 
+class ProcessView(BaseView):
+
+
+    @expose('/showProcess/')
+    @has_access
+    def showProcess(self):
+
+        return self.render_template('/process.html')
+
+
+
+    @expose('/getTableData')
+    def getTableData(self):
+
+        file_obs_E = '/Users/yli120/rfish/Tables/Obs and Pred Sum_E.csv'
+        file_obs_W = '/Users/yli120/rfish/Tables/Obs and Pred Sum_W.csv'
+
+        df_E = pd.read_csv(file_obs_E,usecols=['Year','Observed'])
+        df_E = df_E.rename(index=str, columns={"Year": "E_Year", "Observed": "E_Observed"})
+        df_W = pd.read_csv(file_obs_W,usecols=['Year','Observed'])
+        df_W = df_W.rename(index=str, columns={"Year": "W_Year", "Observed": "W_Observed"})
+        df_total = pd.concat([df_E,df_W],axis=1)
+        #return jsonify(df_E.to_json(orient='records'))
+        return Response(df_total.to_json(orient='records'), mimetype='application/json')
+
+    @expose('/editTableData', methods = ['POST'])
+    def editTableData(self):
+        
+        print(request.form["E_Year"])
+
+        return Response(json.dumps({'status':1}), mimetype='application/json')
+
 
 """
     Application wide 404 error handler
 """
+
+appbuilder.add_view(ProcessView,"prcess", href='/processview/showProcess', category='Process View')
+
+
 @appbuilder.app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html', base_template=appbuilder.base_template, appbuilder=appbuilder), 404
