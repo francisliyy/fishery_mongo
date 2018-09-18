002a86ab0873fdb9833b77c6d0aee09965dae30e
yingxin li <yli120@cate.fiu.edu>
2018-09-18 10:20:49 -0400

    remove set current path

--- app/rScript/fishery_service.r
+++ app/rScript/fishery_service.r
@@ -1,7 +1,7 @@
 
 
 #install.packages("plumber")
-setwd('/Users/yli120/Documents/fisheryPrj/fishery_mongo/app/rScript')
+#setwd('/Users/yli120/Documents/fisheryPrj/fishery_mongo/app/rScript')
 library(plumber)
 r <- plumb("rmongo.r")  # Where 'plumber.R' is the location of the file shown above
 r$run(port=8000)
